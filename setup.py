d615de1eb6d980d6000d63f9b6f9026e98eedddf
Margot Belot <margotbelotmfn@gmail.com>
2025-04-21 16:23:42 +0200

    toml

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Ancient-Language-Processing",
-    version="0.1",
+    version="1.0",
     packages=find_packages(),
     install_requires=[
         # list the dependencies here
